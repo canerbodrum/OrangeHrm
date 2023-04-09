70a7f0e5ead2bb5c4d60d39541b30958bdaceba6
sevdaerdem <123573889+sevdaerdem@users.noreply.github.com>
2023-04-09 22:53:40 +0300

    5 charac

---
+++ src/test/resources/StepDefinitions/MyStepdefs.java
@@ -0,0 +1,9 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+
+public class MyStepdefs {
+    @And("Send to user name five  characters")
+    public void sendToUserNameFiveCharacters() {
+    }
+}
